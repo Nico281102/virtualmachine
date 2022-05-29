583b86a213bd0d19e96170fd5b6d7d105ef7b540
Nico <nicososta28@gmail.com>
2022-05-29 17:11:53 +0200

    rifatto

--- 01_09_21/main.s
+++ 01_09_21/main.s
@@ -1,96 +1,62 @@
-/*Tradurre in assembly ARM la seguente funzione low2upp che prende in input una stringa s di lunghezza l, e sostituisce tutte
-le lettere minuscole in maiuscole.
-Nota che 97 `e il codice ASCII di ’a’ e 122 il codice di ’z’, mentre 32 `e la costante da sottrarre per trasformare minuscole in
-maiuscole.
+/*
 void low2upp(char *s, int l) {
-int i;                            
-char v;                              x0 = 0
-    for (i=0; i < l; i++) {          while( x0 < x3){
-        v = s[i];                     x2 = *(x4 + i)
-        if (v >= 97 && v <= 122)        if( v < 97 || v > 122) goto endif
-            s[i] = v - 32;               else *(x4+1) = x2 -32;
-    }                                   }
+int i;
+char v;
+    for (i=0; i < l; i++) {
+        v = s[i];
+        if (v >= 97 && v <= 122)
+            s[i] = v - 32;
+    }
 }
+ */
 
-1. Tradurre in assembly ARM, usando $s0 per la variabile char v (in verit`a, il byte meno significativo del registro).*/
-   
-   // callee saved: x19 - x27
-   // caller saved: x0  - x15
-   
-// Corretto il salvataggio del LR?
-// Come si utilizza l'AND logico?
-// Differenza tra registri callee saved e caller saved?
-   
-   .global _start
-_start:
+    .text
+    .p2align 2
 
-    //Salvo il LR nello stack
-    str LR, [sp, #-16]!
-    //chiamo la funzione
+    .global low2upp
+    low2upp:
+// Param ( x0: s, w1: l)
+    mov w9, #0 // int i = 0
+
+    cmp w9, w1 //if (i >= l) goto endwhile
+    b.ge endwhile
+    initwhile: 
+
+    ldrb w10, [x0] // v = *s
+
+        cmp w10, 97 //if ( v < 97 ) goto endif
+        b.lt endif
+        cmp w10, 122 //if (v > 122) goto endif
+        b.gt endif
+
+        //than
+        sub w10, w10, #32 // v = v - 32
+        strb w10, [x0] // s[i] = v
+
+        endif:
+        add w9, w9, #1 // ++i
+        add x0, x0, #1 // v = v + 1
+
+    cmp w9, w1 // if( i < l) goto initwhile
+    b.lt initwhile
+    endwhile:
+
+    RET
+            .global _start
+    _start:
+    adr x0, s
+    adr x1, len
+    ldr x1, [x1]
+
     BL low2upp
-    //ripristino il LR
-    ldr LR, [sp], #16
 
 
-    //Salvo il LR nello stack
-    str LR, [sp, #-16]!
-    //chiamo la funzione
-    BL print_string
-    //ripristino il LR
-    ldr LR, [sp], #16
-
-
-    //fine del programma
     mov x0, #0
     mov x8, #93
     svc #0
-   
-   
-   
-    .global low2upp
-low2upp:
-
-    movz w1, #0 //int i = 0;
-    
-    adr x6, len
-    ldr w7, [x6] //x7 = *(len)
-    cmp w1, w7  //if(x1 >= x7) goto endwhile
-    b.ge endwhile
-
-    adr x2, s // x2= s
-    initwhile:
-
-    add x3, x2, x1 // sommo l'offset al puntatore x3 = s+i
-    ldrb w4, [x3] // w4 = *(x3) v = s[i], carico 1 byte
-    
-    /*cmp w4, #97  // if( w4 < 97) goto endif
-    b.lt endif
-    cmp w4, #122 // if(w4 > 122) goto endif
-    b.gt endif*/
-    sub w4, w4, #32 // v = v - 32
-    strb w4, [x3] // s+i = v    
-    add w1, w1, #1
-
-    endif:
-    cmp w1, w7 // if( w1 < w7 ) goto initwhile
-    b.lt initwhile
-
-    endwhile:
-    RET
 
 
-
-    .global print_string
-
-print_string:
-
-    mov x0, #1 //descrittore dello std_out
-    adr x1, s // metto in x1 il puntatore alla stringa
-    mov x2, #12 // metto in x2 la len della stringa
-    mov x8, #0x40 //chiama la write, x8 è il registro per le syscall /*0x40 <=> 64 , 64 <=> 0100 0000 <=> 40 */
-    svc #0
-    RET
-
-.data
-s: .string "stringa"
-len: .word 7
+            .data
+            .p2align 2
+s: .string "CiAo"
+len: .word 4

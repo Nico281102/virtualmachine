1e7039fd241603bb87ac2f7d1fdd2cff88df4498
Nico <nicososta28@gmail.com>
2022-05-29 18:51:05 +0200

    rifatto

--- 10_01_19/main.s
+++ 10_01_19/main.s
@@ -1,71 +1,56 @@
-/*
+/* 
 int copy(char a[], char b[]) {
 int i = 0;
-while (b[i] != 0) {
-a[i] = b[i];
-i++;
-}
+    while (b[i] != 0) {
+    a[i] = b[i];
+    i++;
+    }
 a[i] = 0;
 return i;
-} */
-
-/*
-copy:
-x19 = 0 
-while(x2[x19] != #0){
-    x1[x19] = X2[x19]     
-x19++
 }
  */
 
     .text
     .p2align 2
+
     .global copy
+    copy:
+    // param ( x0 = a, x1 = b)
 
-copy:
+    mov x9, #0 // int i = 0
+    ldrb w10, [x1] // w10 = b[i]
+        cmp w10 , #0 // if( b[i] == 0 ) goto endwhile
+        b.eq endwhile
+        initwhile:
+        strb w10, [x0] // a[i] = b [i]
 
-stp x19, x21 ,[sp, #-16]!
-stp x22, x23 ,[sp, #-16]!
-mov x19, #0
-ldrb w23, [x2]
-cmp w23 , #0
-b.eq endwhile
-initwhile:
-add x21, x1 , x19//, LSL #2 // x21 = x1 + (x19*4)
-add x22, x2 , x19//, LSL #2 // x22 = x2 + (x19*4)
-ldrb w23, [x22]
-strb w23, [x21]
+        add x0, x0, #1 // a = a + 1
+        add x9, x9, #1 // ++i
+        add x1, x1, #1 // b = b + 1
+        ldrb w10, [x1] // w10 = b[i]
+        
+        cmp w10, #0 //if ( b[i] != 0 ) goto initwhile
+        b.ne initwhile
 
-add x19, x19, #1 // i+=1
-cmp w23, #0
-b.ne initwhile
-endwhile:
-ldp x22 , x23 ,[sp], #16
-ldp x19 , x21 ,[sp], #16
+        endwhile:
 
-RET 
+        strb w10, [x0] // a[i] = b [i](0)
 
-    .global _start
-_start:
+        mov x0, x9 //return i
+        RET
+        
+        .global _start
+        _start:
+        adr x0, a
+        adr x1, b
 
-adr x1, a
-adr x2, b
-//str x1, [x7]b 
-//str x2, [x6]
-str LR, [sp, #-16]!
-BL copy
-ldr LR, [sp], #16
-//str x1, [x7]
-//str x2, [x6]
+        BL copy
 
-mov x0, #0
-mov x8, #93
-svc #0
+        mov x0, #0
+        mov x8, #93
+        svc #0
 
-
-    .data
-    .p2align 2
-
-a: .string "ilovecoding" 
-b: .string "keepcalmandcode"
-
+        .data
+        .p2align 2
+a: .space 5
+b: .string "ciao"
